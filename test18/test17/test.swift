8b53f7eb27e48103c6bf8f150ad13570c00c1592
Dai Ishizuka <tennisnx80s@i.softbank.jp>
2019-06-11 12:34:02 +0900

    firstcommit

---
+++ test18/test17/test.swift
@@ -0,0 +1,30 @@
+//
+//  test.swift
+//  test17
+//
+//  Created by 石塚大 on 2019/06/11.
+//  Copyright © 2019 石塚大. All rights reserved.
+//
+
+import UIKit
+
+class test: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
